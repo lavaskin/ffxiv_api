e90fbd7d66b4403d788f186d948b82fe981b8834
lavaskin@gmail.com <lavaskin@gmail.com>
2026-01-12 16:31:46 -0600

    hookup ef and my db context

---
+++ ffxiv_api/Data/AppDbContext.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using ffxiv_api.Models.Entity;
+
+namespace ffxiv_api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(
+		DbContextOptions<AppDbContext> options
+	) : base(options)
+    {
+    }
+
+    public DbSet<DutyModel> Duties { get; set; }
+    public DbSet<MentorRouletteLog> MentorRouletteLogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure DutyModel
+        modelBuilder.Entity<DutyModel>(entity =>
+        {
+            entity.ToTable("duty");
+            entity.HasKey(e => e.DutyId);
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.DutyTypeId).IsRequired(false);
+            entity.Property(e => e.ExpansionId).IsRequired(false);
+            entity.Property(e => e.LevelRequirement).IsRequired();
+            
+        });
+
+        // Configure MentorRouletteLog
+        modelBuilder.Entity<MentorRouletteLog>(entity =>
+        {
+            entity.ToTable("mentor_roulette_log");
+            entity.HasKey(e => e.MentorRouletteLogId);
+            entity.Property(e => e.PlayedJobId).IsRequired();
+            entity.Property(e => e.Notes).IsRequired();
+            entity.Property(e => e.DatePlayed).IsRequired();
+            entity.Property(e => e.SortOrder).IsRequired();
+
+            // Configure foreign key relationship
+            entity.HasOne(e => e.DutyModel)
+                .WithMany()
+                .HasForeignKey(e => e.DutyId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+    }
+}
--- ffxiv_api/Program.cs
+++ ffxiv_api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using ffxiv_api.Data;
+using ffxiv_api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
@@ -7,6 +11,10 @@ builder.Services.AddEndpointsApiExplorer();
 // Add SQL connection string
 var connectionString = builder.Configuration["SQL:ConnectionString"];
 builder.Services.AddSingleton(connectionString ?? throw new InvalidOperationException("Connection string not found"));
+
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
